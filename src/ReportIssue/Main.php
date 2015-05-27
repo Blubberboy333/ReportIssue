9a12d66fc9a816f47f051397db419b20b9117f98
Blubberboy333 <thecowmooman@gmail.com>
2015-05-27 11:59:15 -0400

    Update Main.php

--- src/ReportIssue/Main.php
+++ src/ReportIssue/Main.php
@@ -2,15 +2,16 @@
 
 namespace ReportIssue;
 
+use pocketmine\Server;
+use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\utils\TextFormat;
-use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
 
 class Main extends PluginBase{
-  
   public function onEnable(){
-    $this->getLogger()->info(TextFormat::BLUE . "ReportIssue enabled");
+    $this->getLogger->info(TextFormat::BLUE . "ReportIssue enabled");
   }
   public function onDisable(){
     $this->getLogger->info(TextFormat::RED . "ReportIssue disabled");
@@ -18,17 +19,12 @@ class Main extends PluginBase{
   
   public function onCommand(CommandSender $sender, Command $command, $label, array $args){
     if(strtolower($command->getName()) === "report"){
-      if($sender instanceof Player){
-        if($sender->hasPermission("reportissue.command.report")){
-          $name = $sender->getDisplayName();
-          $sender->sendMessage("[ReportIssue] Your issue has been reported");
-          $this->getLogger()->info(TextFormat::RED .$name. " has reported an issue!!!");
-          $this->getServer()->broadcastMessage("[ReportIssue] " .$name. " needs help!");
-        }else{
-          $sender->sendMessage("You don't have permission to do that!");
-        }
+      if($sender->hasPermission("reportissue") || $sender->hasPermission("reportissue.command") || $sender->hasPermission("reportissue.command.report")){
+        $player = $sender->getDisplayName();
+        $this->getLogger()->info(TextFormat::YELLOW . "[ReportIssue] " .$player. " needs help!");
+        $this->getServer()->broadcastMessage("[ReportIssue] " .$player. " needs help!");
       }else{
-        $sender->sendMessage(TextFormat::RED . "Please run that command in-game");
+        $sender->sendMessage("You don't have permission to do that!");
       }
     }
   }
